0638824cbfb1e98be78819c5c6241b9bebfeeab9
klsova <kane.sova@gmail.com>
2024-09-01 17:47:41 +0300

    Props

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -1,7 +1,22 @@
-const App = () => (
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const nimi = "Pekka"
+  const ika = 10
+
+  return (
   <div>
-    <p>Hello World</p>
+    <h1>Greetings</h1>
+    <Hello name="Maya" age={26 + 10} />
+    <Hello name="Pekka" age={ika} />
   </div>
 )
-
+}
 export default App
